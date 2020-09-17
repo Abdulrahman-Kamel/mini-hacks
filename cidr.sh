1ad5fa94da06fb37f07d1e495c760f4f6b51b2c5
Abdulrahman-Kamel <49864433+Abdulrahman-Kamel@users.noreply.github.com>
2020-09-17 16:23:06 +0200

    Update cidr.sh

--- cidr.sh
+++ cidr.sh
@@ -1,16 +1,28 @@
 args=("$@")
-if [ -z ${args[0]} ]
-then
-printf "%s\n\e[47m\e[31mPlease Enter Company Name\n\e[0m\n"
+
+if [ -z ${args[0]} ];then
+printf "%s\n\n\e[31m Please enter company name: \e[0m" ; read -p '' comapnyName
 else
-printf "%s\n\e[31m========================================\n= Extracting CIDR ${args[0]} in -> CIDR.txt =\n========================================\n\e[0m\n"
-echo "" > CIDR.txt
-amass intel -org ${args[0]} | cut -d , -f 1 > .asn
-for asn in $(cat .asn)
-do
-whois -h whois.radb.net -- "-i origin AS$asn" | grep -Eo "([0-9.]+){4}/[0-9]+" | sort -u >> CIDR.txt
-cat CIDR.txt
-done;
-rm -r .asn;
-printf "%s\n\e[31mCIDR Count --> " && cat CIDR.txt | wc -l && printf "\n\e[0m\n"
+comapnyName=$1
 fi
+
+if [ -z ${args[1]} ];then
+printf "%s\n\n\e[31m Please enter results file: \e[0m" ; read -p '' results
+else
+results=$2
+fi
+
+cidr_func () {
+	printf "%s\n\e[31m========================================\n Extracting CIDR $comapnyName in -> $results \n========================================\n\e[0m\n"
+	touch $results
+	amass intel -org $comapnyName | cut -d , -f 1 > .asn
+	for asn in $(cat .asn)
+	do
+	whois -h whois.radb.net -- "-i origin AS$asn" | grep -Eo "([0-9.]+){4}/[0-9]+" | sort -u >> $results
+	cat $results
+	done;
+}
+
+cidr_func
+rm -r .asn;
+printf "%s\n\e[31m CIDR Count --> " && cat $results | wc -l && printf " in File $results \n\e[0m\n"
